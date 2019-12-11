6f84735ee7da028f35131eae3b99cef576e0a893
vivenz <17706500512@163.com>
2019-12-11 16:11:13 +0800

    1

--- static/dist/js/com.js
+++ static/dist/js/com.js
@@ -1,35 +1,87 @@
-
 $(function () {
-  var windowW = $(window).width()
-  var windowH = $(window).height();
-  var contentH = windowH  - 69 - 48 -87;
-  var content = $(".content").height();
-  $(".middle_bg").height(contentH);
-  $(".middle_bg").css({
-      "overflow-x": "hidden"
-  })
-  $('.middle_bg').perfectScrollbar({
-    cursorwidth: 0
-      });
-  
-  if (windowW < 1366) {
-      $("body").css({
-          "overflow-x": "scroll"
-      })
-  } else {
-      $("body").css({
-          "overflow-x": "hidden"
-      })
-  }
+    var windowW = $(window).width()
+    var windowH = $(window).height();
+    var contentH = windowH - 69 - 48 - 87;
+    var content = $(".content").height();
+    $(".middle_bg").height(contentH);
+    $(".middle_bg").css({
+        "overflow-x": "hidden"
+    })
+    $('.middle_bg').perfectScrollbar({
+        cursorwidth: 0
+    });
 
-  setTimeout(function(){
-      console.log(windowH)
-      $('.slimScrollDiv').height(windowH - 77)
-      $('.slimScrollDiv').css({
-          "overflow-y": "scroll"
-      })
-      $('.slimScrollDiv').perfectScrollbar({
-    cursorwidth: 0
-      });
-  },0)
-})
+    if (windowW < 1366) {
+        $("body").css({
+            "overflow-x": "scroll"
+        });
+    } else {
+        $("body").css({
+            "overflow-x": "hidden"
+        });
+    }
+    $('.main-sidebar_bg').height(windowH - 87)
+    $('.main-sidebar_bg').css({
+        "overflow-x": "hidden",
+        "overflow-y": "scroll"
+    })
+    $('.main-sidebar_bg').perfectScrollbar({
+        cursorwidth: 0
+    });
+    //
+    var browserInfo = getBrowserInfo();
+    $(".treeview ").on('click', function () {
+    
+        if (!browserInfo || browserInfo == 'UC') {
+    
+            $('.main-sidebar_bg').height(windowH - 87)
+            $('.main-sidebar_bg').css({
+                "overflow-y": "scroll"
+            })
+            $('.main-sidebar_bg').perfectScrollbar({
+                cursorwidth: 0
+            });
+        }
+    });
+
+    function getBrowserInfo() {
+        var ua = navigator.userAgent.toLocaleLowerCase();
+        var browserType = null;
+        if (ua.match(/msie/) != null || ua.match(/trident/) != null) {
+            browserType = "IE";
+            browserVersion = ua.match(/msie ([\d.]+)/) != null ? ua.match(/msie ([\d.]+)/)[1] : ua.match(/rv:([\d.]+)/)[1];
+        } else if (ua.match(/firefox/) != null) {
+            browserType = "火狐";
+        } else if (ua.match(/ubrowser/) != null) {
+            browserType = "UC";
+        } else if (ua.match(/opera/) != null) {
+            browserType = "欧朋";
+        } else if (ua.match(/bidubrowser/) != null) {
+            browserType = "百度";
+        } else if (ua.match(/metasr/) != null) {
+            browserType = "搜狗";
+        } else if (ua.match(/tencenttraveler/) != null || ua.match(/qqbrowse/) != null) {
+            browserType = "QQ";
+        } else if (ua.match(/maxthon/) != null) {
+            browserType = "遨游";
+        } else if (ua.match(/chrome/) != null) {
+            var is360 = _mime("type", "application/vnd.chromium.remoting-viewer");
+            if (is360) {
+                browserType = '360';
+            }
+        } else if (ua.match(/safari/) != null) {
+            browserType = "Safari";
+        }
+        return browserType;
+    }
+
+    function _mime(option, value) {
+        var mimeTypes = navigator.mimeTypes;
+        for (var mt in mimeTypes) {
+            if (mimeTypes[mt][option] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+});
