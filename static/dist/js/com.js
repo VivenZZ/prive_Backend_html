986b6fd1e14ba37bbf823eea653acbb561905dc9
vivenz <17706500512@163.com>
2019-12-16 14:00:19 +0800

    修改导航

--- static/dist/js/com.js
+++ static/dist/js/com.js
@@ -7,7 +7,6 @@ $(function () {
     $(".middle_bg").css({
         "overflow-x": "hidden"
     })
-    new PerfectScrollbar('.middle_bg');
 
     if (windowW < 1366) {
         $("body").css({
@@ -21,61 +20,10 @@ $(function () {
     $('.main-sidebar_bg').height(windowH - 87)
     $('.main-sidebar_bg').css({
         "overflow-x": "hidden",
-        "overflow-y": "scroll"
+        "overflow-y": "hidden"
     })
-    new PerfectScrollbar('.main-sidebar_bg');
-    // //
-    var browserInfo = getBrowserInfo();
-    $(".treeview ").on('click', function () {
-    
-        if (!browserInfo || browserInfo == 'UC') {
-    
-            $('.main-sidebar_bg').height(windowH - 87)
-            $('.main-sidebar_bg').css({
-                "overflow-y": "scroll"
-            })
-            new PerfectScrollbar('.main-sidebar_bg');
-        }
+    $("body").mCustomScrollbar({
+        theme:"minimal"
     });
 
-    function getBrowserInfo() {
-        var ua = navigator.userAgent.toLocaleLowerCase();
-        var browserType = null;
-        if (ua.match(/msie/) != null || ua.match(/trident/) != null) {
-            browserType = "IE";
-            browserVersion = ua.match(/msie ([\d.]+)/) != null ? ua.match(/msie ([\d.]+)/)[1] : ua.match(/rv:([\d.]+)/)[1];
-        } else if (ua.match(/firefox/) != null) {
-            browserType = "火狐";
-        } else if (ua.match(/ubrowser/) != null) {
-            browserType = "UC";
-        } else if (ua.match(/opera/) != null) {
-            browserType = "欧朋";
-        } else if (ua.match(/bidubrowser/) != null) {
-            browserType = "百度";
-        } else if (ua.match(/metasr/) != null) {
-            browserType = "搜狗";
-        } else if (ua.match(/tencenttraveler/) != null || ua.match(/qqbrowse/) != null) {
-            browserType = "QQ";
-        } else if (ua.match(/maxthon/) != null) {
-            browserType = "遨游";
-        } else if (ua.match(/chrome/) != null) {
-            var is360 = _mime("type", "application/vnd.chromium.remoting-viewer");
-            if (is360) {
-                browserType = '360';
-            }
-        } else if (ua.match(/safari/) != null) {
-            browserType = "Safari";
-        }
-        return browserType;
-    }
-
-    function _mime(option, value) {
-        var mimeTypes = navigator.mimeTypes;
-        for (var mt in mimeTypes) {
-            if (mimeTypes[mt][option] == value) {
-                return true;
-            }
-        }
-        return false;
-    }
 });
